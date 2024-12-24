124db8ab5c5904141a3cc7339ef8b1b644cf8427
worker-one <konst.verner@gmail.com>
2024-12-24 02:26:09 +0100

    wip

--- scripts/excel_to_db.py
+++ scripts/excel_to_db.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+
 from real_estate_telegram_bot.db.crud import upsert_project
 from real_estate_telegram_bot.db.database import create_tables
 from real_estate_telegram_bot.db.models import Project
@@ -7,14 +8,16 @@ from real_estate_telegram_bot.db.models import Project
 if __name__ == '__main__':
 
     # Step 1: Read the Excel file
-    excel_file_path = './data/Projects_2000-2025_14 oct_short.xlsx'
+    excel_file_path = './data/Projects_2000-2025_17 dec_short.xlsx'
     df = pd.read_excel(excel_file_path, na_values=['#N/A','NA', 'N/A', 'nan', 'NaN', '', 'NaT'], keep_default_na=False)
     df = df.where(pd.notnull(df), None)
     df = df.replace({np.nan: None})
-    create_tables()
+    #create_tables()
 
     # Step 2: Iterate over DataFrame rows and create Project objects
     for index, row in df.iterrows():
+        if index % 100 == 0:
+            print(f"{index/len(df)*100:.2f}% done")
         project = Project(
             project_id=row['project_id'],
             project_name=row['project_name'],
